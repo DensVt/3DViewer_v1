16745ae0925380d09e208615c61d549f19c09793
AlexanderKuznetsov13 <Agk13145@icloud.com>
2023-10-10 17:48:58 +0300

    matrix all added

---
+++ src/backend/common/s21_matrix.h
@@ -0,0 +1,26 @@
+#ifndef SRC_S21_MATRIX_H_
+#define SRC_S21_MATRIX_H_
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SUCCESS 1
+#define FAILURE 0
+
+typedef struct matrix_struct {
+  double **matrix;
+  int rows;
+  int columns;
+} matrix_t;
+
+enum {
+  OK = 0,
+  INCORRECT_MATRIX = 1,
+  CALC_ERROR = 2,
+};
+
+int s21_create_matrix(int rows, int columns, matrix_t *result);
+void s21_remove_matrix(matrix_t *A);
+
+#endif  // SRC_S21_MATRIX_H_
